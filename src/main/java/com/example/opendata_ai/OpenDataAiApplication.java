1260737beef2bf6d605393931905791467c68a60
m.mekhmonkuziev <@m.mekhmonkuziev>
2023-01-28 11:10:55 +0500

    bug fixed

--- src/main/java/com/example/opendata_ai/OpenDataAiApplication.java
+++ src/main/java/com/example/opendata_ai/OpenDataAiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenDataAiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenDataAiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenDataAiApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/opendata_ai/OpenDataAiApplicationTests.java
+++ src/test/java/com/example/opendata_ai/OpenDataAiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OpenDataAiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
