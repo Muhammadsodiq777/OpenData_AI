6e0598fbd554153f12a3d4861a82de7790a0ddba
m.mekhmonkuziev <@m.mekhmonkuziev>
2023-01-28 11:07:21 +0500

    Initial commit

---
+++ src/main/java/com/example/opendata_ai/OpenDataAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.opendata_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDataAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDataAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opendata_ai/OpenDataAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opendata_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDataAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
